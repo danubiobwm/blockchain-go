8b0d65488f72152c39224a857e302c55ed0b5d5a
José Danubio de Araujo <danubio.bwm@gmail.com>
2024-10-27 01:01:22 -0300

    blockchain

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/danubiobwm/blockchain-go
 
 go 1.23.1
+
+require rsc.io/quote/v3 v3.1.0
+
+require (
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/danubiobwm/blockchain-go/blockchain"
+)
+
+func main() {
+
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("Second Block after Genesis")
+	chain.AddBlock("Third Block after Genesis")
+
+	for _, block := range chain.Blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
